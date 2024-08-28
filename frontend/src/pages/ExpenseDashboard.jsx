7359bce002ee47ffdf34273ec3b1d4ecf3e594b2
Abstract-ak <ash.king0205@gmail.com>
2024-08-28 16:38:43 +0530

    Popup form developed

---
+++ frontend/src/pages/ExpenseDashboard.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import "../styles/Dashboard/Dashboard.css";
+import ExpenseForm from "./ExpenseForm";
+
+export default function ExpenseDashboard() {
+  return (
+    <>
+      <div className="displayHeading">
+        <h2>Expense Traker</h2>
+        <h1 className="user-name">Hi🖐 AKASH</h1>
+      </div>
+
+      {/* button to create expense */}
+
+      <div className="Expense-table">
+        {/* expense component */}
+        <ExpenseForm />
+
+        <table>
+          <thead>
+            <tr>
+              {/* <th>Serial No</th> */}
+              <th>Serial No.</th>
+              <th>UserName</th>
+              <th>Gender</th>
+              <th>Mobile No</th>
+              <th>Email ID</th>
+              <th>Authorization</th>
+              <th>Status</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          {/* <tbody>
+            {tableData?.length > 0 &&
+              tableData?.map((user, index) => (
+                <tr key={index}>
+                  <td>{(page - 1) * count + index + 1}</td>
+                  <td>{user.user_name}</td>
+                  <td>{user.gender}</td>
+                  <td>{user.contact_no}</td>
+                  <td>{user.email_id}</td>
+                  <td>{user.role}</td>
+                  <td
+                    className={`${
+                      user.status === "ACTIVE"
+                        ? "text-green-500"
+                        : "text-red-500"
+                    }`}
+                  >
+                    {user.status}
+                  </td>
+                  <td className="px-6 py-4">
+                    <a
+                      onClick={() => {
+                        navigate("/configuration/user/edit");
+                        setParameter(user.id);
+                      }}
+                      className="font-medium text-blue-600 dark:text-blue-500 cursor-pointer"
+                    >
+                      Edit
+                    </a>
+                  </td>
+                </tr>
+              ))}
+          </tbody> */}
+        </table>
+      </div>
+    </>
+  );
+}
--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,16 +1,18 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import LoginPage from '../pages/LoginPage';
-import FormPage from '../pages/FormPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginPage from "../pages/LoginPage";
+import Dashboard from "../pages/ExpenseDashboard";
+import ExpenseForm from "../pages/ExpenseForm";
 
 const AppRoutes = () => {
-    return (
-        <Router>
-            <Routes>
-                <Route path="/" element={<LoginPage />} />
-                <Route path="/form" element={<FormPage />} />
-            </Routes>
-        </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/dashboard" element={<ExpenseForm />} /> */}
+      </Routes>
+    </Router>
+  );
 };
 
 export default AppRoutes;
